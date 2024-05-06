06decd663c8f3996c6cef4451c28d738efdfd252
Sarvan Maurya <sarvanmaurya037@gmail.com>
2024-05-06 16:46:42 +0530

    first commit 3

--- src/Url.jsx
+++ src/Url.jsx
@@ -1,2 +1,2 @@
-//export const BACKEND = "https://mernfrontent.onrender.com"
-export const BACKEND = "http://localhost:5000"
+export const BACKEND = "https://frontendmern-skme.onrender.com"
+//export const BACKEND = "http://localhost:5000"
